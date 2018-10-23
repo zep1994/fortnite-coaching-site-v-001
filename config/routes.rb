67f87131c0473a79951e29c6c144c28e0a00e974
zep1994 <snaxbags@protonmail.com>
2018-10-23 14:02:35 -0500

    Add signup for User. Add user controller, model and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :users, :only => [:new, :create]
+
   resources :meetings do
     resources :students
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_22_215450) do
+ActiveRecord::Schema.define(version: 2018_10_23_172629) do
 
   create_table "meetings", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2018_10_22_215450) do
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "meeting_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
